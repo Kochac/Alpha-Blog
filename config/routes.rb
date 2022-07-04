641b76d0e2969de6c92af2d9acbde4043879a993
Kochac <maks.absalon@gmail.com>
2022-07-04 02:33:10 +0200

    init

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "pages#home"
+
+  get '/about', to: "pages#about"
+
+  resources :articles, only: [:show,:index,:new,:create]
+
+end
